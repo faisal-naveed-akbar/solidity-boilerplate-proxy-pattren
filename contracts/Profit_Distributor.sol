a8591fc3396d559a2b3d3ebc8ddd1c7e83cb6981
saqib <malik.saqib@rnssol.com>
2021-10-05 17:22:53 +0500

    profit distributor updated

--- contracts/Profit_Distributor.sol
+++ contracts/Profit_Distributor.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 
 contract Profit_Distributor is Initializable{
